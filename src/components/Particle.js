bc7d2cc77f226e4a6fde8907e3981e601a1935e6
gauravjha0711 <gauravjha0711@gmail.com>
2025-04-21 20:14:53 +0530

    update particle js

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -5,46 +5,84 @@ function Particle() {
   return (
     <Particles
       id="tsparticles"
-      params={{
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: -1,
+        },
         particles: {
           number: {
-            value: 160,
+            value: 120,
             density: {
               enable: true,
-              value_area: 1500,
+              area: 800,
             },
           },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
+          color: {
+            value: ["#00ffff", "#ff00ff", "#ffffff"],
           },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
+          shape: {
+            type: "circle",
           },
           opacity: {
+            value: 0.6,
+            random: true,
             anim: {
               enable: true,
               speed: 1,
-              opacity_min: 0.05,
+              opacity_min: 0.2,
+              sync: false,
+            },
+          },
+          size: {
+            value: { min: 1, max: 3 },
+            random: true,
+            anim: {
+              enable: true,
+              speed: 2,
+              size_min: 0.3,
+              sync: false,
+            },
+          },
+          move: {
+            enable: true,
+            speed: 0.5,
+            direction: "none",
+            random: true,
+            straight: false,
+            outModes: {
+              default: "out",
+            },
+            attract: {
+              enable: true,
+              rotateX: 600,
+              rotateY: 1200,
             },
           },
         },
         interactivity: {
           events: {
-            onclick: {
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            onClick: {
               enable: true,
               mode: "push",
             },
           },
           modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
             push: {
-              particles_nb: 1,
+              quantity: 2,
             },
           },
+        },
+        background: {
+          color: "transparent",
         },
         retina_detect: true,
       }}
