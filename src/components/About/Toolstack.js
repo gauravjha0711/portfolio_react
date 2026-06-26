45050870786b484833664d45c1a95fd49e3f2df8
gauravjha0711 <gauravjha0711@gmail.com>
2026-06-26 17:08:48 +0530

    Tools Stack update

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,35 +1,96 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { FaCode } from "react-icons/fa";
+
 import {
   SiVisualstudiocode,
   SiPostman,
-  SiVercel,
   SiWindows,
-  SiNetlify,
+  SiDocker,
+  SiKubernetes,
+  SiRedis,
+  SiRabbitmq,
+  SiGithub,
+  SiGit,
+  SiLinux,
+  SiSwagger,
+  SiMicrosoftsqlserver,
+  SiAmazon,
+  SiMicrosoftazure,
+  SiVisualstudio,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+
+      {/* Operating Systems */}
       <Col xs={4} md={2} className="tech-icons">
-        <SiWindows />
+        <SiWindows title="Windows" />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
+        <SiLinux title="Linux" />
       </Col>
+
+      {/* IDEs */}
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+        <SiVisualstudiocode title="VS Code" />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <SiNetlify />
+        <SiVisualstudio title="Visual Studio" />
       </Col>
+
+      {/* Version Control */}
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiGit title="Git" />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <FaCode />
+        <SiGithub title="GitHub" />
       </Col>
+
+      {/* API Tools */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman title="Postman" />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSwagger title="Swagger" />
+      </Col>
+
+      {/* Databases */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftsqlserver title="SQL Server" />
+      </Col>
+
+      {/* DevOps */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDocker title="Docker" />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiKubernetes title="Kubernetes" />
+      </Col>
+
+      {/* Microservices */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis title="Redis" />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRabbitmq title="RabbitMQ" />
+      </Col>
+
+      {/* Cloud */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazon title="AWS" />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftazure title="Azure" />
+      </Col>
+
     </Row>
   );
 }
